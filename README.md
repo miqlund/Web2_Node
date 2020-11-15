418a8146fe50ad541892391e1d1ec3e38cb77b5f
Mikko Haikonen <Mikko.Haikonen@edu.savonia.fi>
2020-11-15 15:08:43 +0200

    Versio 2.0
    Lisättiin tehtävät 4-9. Asiakkaan lisääminen vaatii vielä muokkaamista ja frontendin yhdistämistä.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 Tämä projekti on Savonian Web-ohjelmoinnin jatkokurssin tehtävien varten luotu repository. Koodien pohjat ovat tulleet Savonia lehtoreilta, joita allekirjoittanut on muokannut. Projekti on vain oppimistarkoitukseen, eikä mihinkaan muuhun.
 
-1. Muokkaa customerController.js tiedostoon SQL-serverisi tiedot, jotta hakea tietoja (rivit 5-10).
+Tehtävät 2 ja 3
+
+1. Muokkaa customerController.js tiedostoon SQL-serverisi tiedot, josta voit hakea tietoja (rivit 5-10).
 2. Lataa node_modules CMD-komenolla npm install
 3. Avaa serveri CMD-komennolla npm run start
-4. Avaa käyttöliittymä selaimeen osoitteessa http://127.0.0.1:3002/ (localhost-osoite) tai suoraan node.html tiedosta.
+4. Avaa käyttöliittymä suoraan node.html tiedosta.
 5. Kaikki asiakkaat saa haettua Hae-nappia painamalla.
 6. Parametrien haku onnistuu kirjoittamalla kenttään ja painamalla Hae-nappia.
--- customerController.js
+++ customerController.js
@@ -2,20 +2,23 @@
 
 var mysql = require("mysql");
 
+// Luodaan yhteys tietokantaan
 var connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "Jalkapallo20",
-  database: "asiakas",
+  user: "root",                 // OMA KÄYTTÄJÄ
+  password: "Jalkapallo20",     // OMA SALASANA
+  database: "asiakas",          // OMA TIETOKANNAN NIMI
 });
 
 module.exports = {
+
+  
   fetchTypes: function (req, res) {
     connection.query(
       "SELECT Avain, Lyhenne, Selite FROM Asiakastyyppi",
       function (error, results, fields) {
         if (error) {
-          console.log("Virhe haettaessa dataa asiakas-taulusta, syy " + error);
+          console.log("Virhe haettaessa dataa, syy " + error);
           res.send({ status: 500, error: error, response: null });
         } else {
           console.log("Data = " + JSON.stringify(results));
@@ -25,20 +28,21 @@ module.exports = {
     );
   },
 
+  // Tehtävä 2
   // Haetaan kaikki asiakkaat
   fetchAll: function (req, res) {
-    var query = "SELECT * FROM asiakas WHERE 1=1";
+    var query = "SELECT AVAIN, NIMI, OSOITE, POSTINRO, POSTITMP, LUONTIPVM, ASTY_AVAIN FROM asiakas WHERE 1=1";
 
     if (req.query.length != 0) {
       for (var key in req.query) {
         query += " AND " + key + " LIKE '" + req.query[key] + "%'";
       }
     } else {
-      query = "SELECT * FROM asiakas WHERE 1=1";
+      query = "SELECT AVAIN, NIMI, OSOITE, POSTINRO, POSTITMP, LUONTIPVM, ASTY_AVAIN FROM asiakas WHERE 1=1";
     }
     connection.query(query, function (error, results, fields) {
       if (error) {
-        console.log("Virhe haettaessa dataa asiakas-taulusta, syy " + error);
+        console.log("Virhe haettaessa dataa, syy " + error);
         res.send({ status: 500, error: error, response: null });
       } else {
         res.json(results);
@@ -46,13 +50,55 @@ module.exports = {
     });
   },
 
-  create: function (req, res) {
-    res.send("Kutsuttiin create");
+
+  // Tehtävä 4 ja 9
+  // Lisätään asiakas tietokantaan ja tarkistetaan virheet
+  create: function(req, res){
+		
+    console.log("Create = " + JSON.stringify(req.body));
+
+  if ( req.body.nimi == "" || req.body.osoite == "" )
+  {
+        res.send({"status": "NOT OK", "error": "Kenttä on tyhjä"}); 
+  }
+  else 
+  {
+    var sql = "INSERT INTO asiakas(NIMI, OSOITE, POSTINRO, POSTITMP, LUONTIPVM, ASTY_AVAIN) VALUES ('" + req.body.nimi + "', '" + req.body.osoite + "', '" + req.body.postinro + "', '" + req.body.postitmp + "', '" + "11.11.2020', '" + req.body.asty_avain + "')";
+    
+    console.log("sql=" + sql);
+    
+    connection.query(sql, function(error, results, fields){
+      if ( error ){
+        console.log("Virhe: " + error);
+        res.send({"status": "Kenttä on tyhjä", "error": error, "response": null}); 
+      }
+      else
+      {					
+        res.send({"status": "OK", "error": ""}); 
+      }
+    });
+  }
   },
+
 
   update: function (req, res) {},
 
+  // Tehtävä 6
+  // Poistetaan asiakas tietokannasta
   delete: function (req, res) {
-    res.send("Kutsuttiin delete");
-  },
+    console.log("Poistettiin id: " + JSON.stringify(req.params));
+    
+  var sql = "DELETE FROM `asiakas` WHERE `AVAIN`='" + req.params.id + "'" ;
+
+  connection.query(sql, function(error, results, fields){
+    if ( error ){
+      console.log("Asiakkaan poistossa virhe: " + error);
+      res.send({"status": 234, "error": error, "response": null}); 
+    }
+    else
+    {
+      res.json(results);
+    }
+});
+}
 };
